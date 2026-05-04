9c31eafdf9e755e2c8042facac54cc87da9b5d34
aledma74 <aledma74@naver.com>
2026-05-05 00:52:50 +0900

    Apps SDK UI

--- widgets/src/App.tsx
+++ widgets/src/App.tsx
@@ -1,7 +1,18 @@
-import './App.css'
+import { Alert } from "@openai/apps-sdk-ui/components/Alert";
+import { Button } from "@openai/apps-sdk-ui/components/Button";
 
 function App() {
-  return <>hi</>;
+  return (
+    <Alert
+      actions={
+        <Button color="primary" pill variant="soft">
+          Dismiss
+        </Button>
+      }
+      description="We'll be offline 2 - 4 AM UTC on July 14 while we upgrade our database."
+      title="Scheduled maintenance"
+    />
+  );
 }
 
 export default App
--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 const FULL_URL = "https://server.aledmana.workers.dev/";
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     base: mode === "production" ? FULL_URL : undefined,
     build: {
       outDir: "../server/dist",
